5ccb81f3d612195b09f2e1d9f38185bcf49ac87a
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2022-09-29 13:55:27 +0200

    get minute hour and second elements

---
+++ index.js
@@ -0,0 +1,6 @@
+const hourEl = document.querySelector(".hour");
+console.log(hourEl);
+const minuteEl = document.querySelector(".minute");
+console.log(minuteEl);
+const secondEl = document.querySelector(".second");
+console.log(secondEl);
