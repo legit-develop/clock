9ec10fa20be9d724f564d6f7b87e200a8f8a1ec6
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2022-09-30 17:41:45 +0200

    add timeout function

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const minuteEl = document.querySelector(".minute");
 console.log(minuteEl);
 const secondEl = document.querySelector(".second");
 console.log(secondEl);
+
+function updateClock() {
+  const currentDate = new Date();
+  console.log(currentDate);
+  setTimeout(updateClock, 1000);
+  console.log(currentDate);
+  const hour = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+  const seconds = currentDate.getSeconds();
+  console.log(hour, minutes, seconds);
+}
+
+updateClock();
