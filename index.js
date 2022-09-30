3bb38aecf1fa160d4b2f4c6fd1e23e1a2d091286
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2022-09-30 17:56:58 +0200

    complete clock functionality

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ function updateClock() {
   const minutes = currentDate.getMinutes();
   const seconds = currentDate.getSeconds();
   console.log(hour, minutes, seconds);
+  const hourDeg = (hour / 12) * 360;
+  hourEl.style.transform = `rotate(${hourDeg}deg)`;
+  const minuteDeg = (minutes / 60) * 360;
+  minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
+  const secondDeg = (seconds / 60) * 360;
+  secondEl.style.transform = `rotate(${secondDeg}deg)`;
 }
 
 updateClock();
